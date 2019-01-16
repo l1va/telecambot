5b2b3c67fc9803d9d97a30303aed548eb8fdfd93
leonVrashitov <student.ufa.vr@gmail.com>
2019-01-16 16:20:10 +0000

    get rid of proxy

--- webcam.py
+++ webcam.py
@@ -14,30 +14,29 @@ def exit_gracefully(signum, frame):
 
 def infinite_webcam():
 
+	while repeat:
+		try:
+			webcam = cv2.VideoCapture(-1)
+			while repeat:
+				ok, img = webcam.read()
+				if ok:
+					cv2.imwrite("img.png", img)
+				else:
+					print("failed to read image")
+					break
+				time.sleep(5)
+				# to skip cache: the av-based linux capture code is using an internal fifo (5 frames, iirc) (c)
+				for j in range(1, 10):
+					webcam.read()
 
-	try:
-		webcam = cv2.VideoCapture(-1)
-		print(webcam)
-		while repeat:
-			ok, img = webcam.read()
-			if ok:
-				cv2.imwrite("img.png", img)
-			else:
-				print("failed to read image")
-				break
+			webcam.release()
+			cv2.destroyAllWindows()
 			time.sleep(5)
-			# to skip cache: the av-based linux capture code is using an internal fifo (5 frames, iirc) (c)
-			for j in range(1, 10):
-				webcam.read()
 
-		webcam.release()
-		cv2.destroyAllWindows()
-		time.sleep(5)
-
-		print("webcam done")
-	except Exception:
-		logging.error("webcam error:" + traceback.format_exc())
-		time.sleep(5)
+			print("webcam done")
+		except Exception:
+			logging.error("webcam error:" + traceback.format_exc())
+			time.sleep(5)
 
 
 def run_webcam():
@@ -47,5 +46,4 @@ def run_webcam():
 
 
 if __name__ == "__main__":
-    while True:
-	        run_webcam()
+        run_webcam()
